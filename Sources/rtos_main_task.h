6df1bd8584db5f97448a920dad5d028ed228ddbb
bobby <bobby1879@centrum.cz>
2015-06-05 12:56:33 -0700

    a

---
+++ Sources/rtos_main_task.h
@@ -0,0 +1,84 @@
+/* ###################################################################
+**     Filename    : rtos_main_task.h
+**     Project     : K64
+**     Processor   : MK64FN1M0VLL12
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2015-06-05, 12:53, # CodeGen: 1
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         main_task - void main_task(os_task_param_t task_init_data);
+**
+** ###################################################################*/
+/*!
+** @file rtos_main_task.h
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup rtos_main_task_module rtos_main_task module documentation
+**  @{
+*/         
+
+#ifndef __rtos_main_task_H
+#define __rtos_main_task_H
+/* MODULE rtos_main_task */
+
+#include "fsl_device_registers.h"
+#include "clockMan1.h"
+#include "pin_init.h"
+#include "osa1.h"
+#include "mqx_ksdk.h"
+#include "uart1.h"
+#include "fsl_mpu1.h"
+#include "fsl_hwtimer1.h"
+#include "MainTask.h"
+#include "uartCom1.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+/*
+** ===================================================================
+**     Event       :  main_task (module rtos_main_task)
+**
+**     Component   :  MainTask [OS_Task]
+*/
+/*!
+**     @brief
+**         RTOS task routine. The routine is generated into os_tasks.c
+**         file.
+**     @param
+**         task_init_data  - Parameter to be passed to the
+**         task when it is created.
+*/
+/* ===================================================================*/
+void main_task(os_task_param_t task_init_data);
+
+
+/* END rtos_main_task */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+#endif 
+/* ifndef __rtos_main_task_H*/
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.5 [05.21]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
